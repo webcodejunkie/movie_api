f7659de6892ad4e69695828835e9ce248c2e5a32
webcodejunkie <webcodejunkie@gmail.com>
2021-09-11 03:05:30 -0400

    enviormental variable added for heroku/security

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const Models = require('./models.js');
 const Movies = Models.Movie;
 const Users = Models.User;
 
-mongoose.connect('mongodb://localhost:27017/skullifydatabase', { useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // include express module with other middleware apps
 const express = require('express'),
