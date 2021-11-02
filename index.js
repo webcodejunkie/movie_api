124e01eeaa104499a5bdf9bae2f0874ae9dc3dda
webcodejunkie <webcodejunkie@gmail.com>
2021-11-01 21:21:29 -0400

    update on CORS

--- index.js
+++ index.js
@@ -22,7 +22,21 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
+
+let allowedOrigins = ['skullify.netlify.app', 'http://localhost:1234'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      let message = 'The CORS policy for this application does not allow acess from origin' + origin;
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
+}));
+
+
 
 let auth = require('./auth')(app);
 
